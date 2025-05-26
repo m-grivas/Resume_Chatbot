1780102531ca93aabceb002396dd1fc6ddb32ee5
micha.gr06@gmail.com <80446823+m-grivas@users.noreply.github.com>
2025-05-26 18:28:12 +0300

    Return correct answer at api.py from keyword_mapper.py

--- backend/api.py
+++ backend/api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from pydantic import BaseModel
+import keyword_mapper
 
 class Query(BaseModel):
     question: str
@@ -16,7 +17,7 @@ app = FastAPI()
 @app.post("/", response_model=Answer)
 def post_query(request: Query):
     print(request.question)
-    return {"message":request.question}
+    return {"message":keyword_mapper.find_best_response("keywords.txt", request.question)}
 
 
 # if __name__ == "__main__":
