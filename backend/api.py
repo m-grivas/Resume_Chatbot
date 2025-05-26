c361601738c824d67ab41903f034439955848afe
micha.gr06@gmail.com <80446823+m-grivas@users.noreply.github.com>
2025-05-26 18:15:07 +0300

    Basic api.py with a post endpoint

---
+++ backend/api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from pydantic import BaseModel
+
+class Query(BaseModel):
+    question: str
+
+class Answer(BaseModel):
+    message: str
+
+
+app = FastAPI()
+
+#Post endpoint that gets user question and returns it back
+@app.post("/", response_model=Answer)
+def post_query(request: Query):
+    print(request.question)
+    return {"message":request.question}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
