8679685d89e8b1e7ef8cfa166cca9b3da916185c
micha.gr06@gmail.com <80446823+m-grivas@users.noreply.github.com>
2025-05-26 18:17:04 +0300

    Added keyword_mapper.py and a list with 7 answers

---
+++ backend/keyword_mapper.py
@@ -0,0 +1,10 @@
+#List with basic responses
+responses = [
+    "Hi, I am Mike, a computer science student at Computer Science Department of University of Crete. I am interested in AI technologies and web applications.", #whoami
+    "I am hard working, willing to help but also strong opinionated and competitive.", #personality
+    "I have advanced python and cpp knowledge. Moreover, I am familiar with arduino and EV3 robot programing.", #skills
+    "I have no prior work experience.", #work
+    "I am studying at Computer Science Department of University of Crete", #studies
+    "I have participated in many mathematics and robotics competitions, with distinctions in many of them.", #projects-achievements
+    "I love sailing, playing chess, going to the gym and swimming." #hobbies
+]
