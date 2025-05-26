f87a127d75988a121a01c4dd307681e8e7745d6b
micha.gr06@gmail.com <80446823+m-grivas@users.noreply.github.com>
2025-05-26 18:56:24 +0300

    added handleSubmit function to post user question to api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,24 @@
 import { useState } from 'react'
 import api from './api'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [question, setQuestion] = useState("");
+  const [response, setResponse] = useState("");
+  
+  // to post question(=text) to api and get answer
+  const handleSubmit = async (text) => {
+    try {
+      const res = await api.post("/", { question: text });
+      setResponse(res.data.message);
+    } catch (err) {
+      console.error("API error:", err);
+      setResponse("Error occurred");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <></>
   )
 }
 
