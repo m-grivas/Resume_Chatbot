8481602c54538ab1bb001d53bc28b0b256fed660
micha.gr06@gmail.com <80446823+m-grivas@users.noreply.github.com>
2025-05-26 18:21:12 +0300

    Added load_keywords function to load content from keywords.txt into an array

--- backend/keyword_mapper.py
+++ backend/keyword_mapper.py
@@ -8,3 +8,34 @@ responses = [
     "I have participated in many mathematics and robotics competitions, with distinctions in many of them.", #projects-achievements
     "I love sailing, playing chess, going to the gym and swimming." #hobbies
 ]
+
+
+#function to read content from the file and put it in a 2d jagged array by groups
+def load_keywords(file_name):
+    #setup jagged array for keywords
+    key_array = []
+    current_group = []
+
+    #open file with keywords
+    with open(file_name, "r") as file:
+        for line in file:
+            #check if "#" in line
+            if "#" in line:
+                #append group to key_array and initialize it for the next group
+                if current_group:
+                    key_array.append(current_group)
+                    current_group = []
+            
+            #if empty line dont include it in the array
+            elif line.strip() == '':
+                continue
+            
+            else:
+                #append line to group
+                current_group.append(line.strip())
+    
+    #append last group
+    key_array.append(current_group)
+
+    return key_array
+
